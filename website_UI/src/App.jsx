9a39105a1fecff5eb673b76cca40e54e6f8b1709
Ankit Kumar <ankitkumar7017751846@gmail.com>
2026-04-28 01:31:31 +0530

    feature: implement header with navbar, logo and basic styling

---
+++ website_UI/src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import Tessera_logo from './assets/Tessera_logo.png'
+
+const App = () => {
+  return (
+    <>
+    <header>
+     
+      <nav className='navbar'>
+         <img className='logo' src={Tessera_logo} alt="logo"  />
+      <ul>
+        <li><a href="">Home</a></li>
+        <li><a href="">Issues</a></li>
+        <li><a href="">Our Motive</a></li>
+      </ul>
+      <button className='btn'>Login</button>
+    </nav>
+    </header>
+  </>
+  )
+}
+
+export default App
