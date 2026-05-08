e9ebedc4150284424972c34c66f0dfb9f3272201
Surya <suryaprakashsingh1705@gmail.com>
2026-05-08 21:09:07 +0530

    final work done

--- tessera_project/src/App.jsx
+++ tessera_project/src/App.jsx
@@ -1,30 +1,3 @@
-// import { Routes,Route } from 'react-router-dom'
-// import CityIntelligence from './pages/Cityintelligence/index'
-// import Home from './pages/Home'
-// import Issue from './pages/Issue'
-// import Issuepage2 from './pages/Issuepage2'
-// import Issuepage3 from './pages/Issuepage3'
-// import Report from './pages/Report'
-
-
-// const App = () => {
-//   return (
-//     <Routes>
-//       <Route path='/' element={<Home />} />
-//       <Route path='/city-intelligence' element={<CityIntelligence />} />
-//       <Route path='/report' element={<Report />} />
-//       <Route path='/issue' element={<Issue />} />
-//       <Route path='/issue/step2' element={<Issuepage2 />} />
-//       <Route path='/issue/step3' element={<Issuepage3 />} />
-//       <Route path='/report' element={<Report />} /> 
-      
-//     </Routes>
-
-
-//   )
-// }
-// export default App
-
 import { Routes, Route } from "react-router-dom";
 
 import CityIntelligence from "./pages/Cityintelligence/index";
@@ -32,7 +5,6 @@ import Home from "./pages/Home";
 import Issue from "./pages/Issue";
 import Issuepage2 from "./pages/Issuepage2";
 import Issuepage3 from "./pages/Issuepage3";
-import Report from "./pages/Report";
 
 const App = () => {
   return (
@@ -46,9 +18,6 @@ const App = () => {
         path="/city-intelligence"
         element={<CityIntelligence />}
       />
-
-      {/* Report Page */}
-      <Route path="/report" element={<Report />} />
 
       {/* Issue Reporting Flow */}
       <Route path="/issue" element={<Issue />} />
